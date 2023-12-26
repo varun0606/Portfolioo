45585f9142506d6cb71254e2f90eb30c90f9b4ee
Varun Naik <98699813+varun0606@users.noreply.github.com>
2023-12-26 09:42:08 +0530

    Update Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer class="dark:bg-gray-900 mt-20 ">
+        <footer class="dark:bg-gray-900 mt-auto ">
             <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
